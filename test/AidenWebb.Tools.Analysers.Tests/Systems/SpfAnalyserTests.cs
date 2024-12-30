993f07f9ab3287232981cdc0a024812d4d4d7708
Aiden Arnkels-Webb <Aiden@Aidenwebb.com>
2024-12-30 15:31:13 +0000

    Move logic in to SpfRecordBase class

--- test/AidenWebb.Tools.Analysers.Tests/Systems/SpfAnalyserTests.cs
+++ test/AidenWebb.Tools.Analysers.Tests/Systems/SpfAnalyserTests.cs
@@ -9,14 +9,17 @@ public class SpfAnalyserTests
     {
         // Arrange
         SpfRecord? spfRecord;
+        var recordText =
+            "v=spf1 include:spf.protection.outlook.com include:spf.mtasv.net include:spf.uk.exclaimer.net include:sendgrid.net ip4:104.21.71.145/32 include:spf.sendinblue.com mx -all"; 
         
         // Act
         SpfRecord.TryParse(
-            "v=spf1 include:spf.protection.outlook.com include:spf.mtasv.net include:spf.uk.exclaimer.net include:sendgrid.net ip4:104.21.71.145/32 include:spf.sendinblue.com mx -all",
+            recordText,
             out spfRecord);
 
         // Assert
         Assert.NotNull(spfRecord);
         Assert.NotEmpty(spfRecord.Terms);
+        Assert.Equal(recordText, spfRecord.ToString());
     }
 }
